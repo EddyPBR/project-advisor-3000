089ffb250413dbfee7619f0ca096973db9f22091
EddyPBR <edvaldojunior1310@gmail.com>
2020-04-14 18:01:48 -0300

    adding a new route authenticate

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 module.exports = {
 
@@ -7,17 +8,34 @@ module.exports = {
 
     try {
       if (await User.findOne({ email }))
-        return response.status(400).send({ error: "User already exists" });
+       return response.status(400).send({ error: "User already exists" });
 
       const user = await User.create(request.body);
-      
+
       // TO NOT RETURN THE PASSWORD
       user.password = undefined;
 
       return response.send(user);
     } catch (error) {
-      return response.status(400).send({ error: "error: user not created." });
+      return response.status(400).send({ error: "User not created." });
     }
+  },
+
+  async authenticate (request, response) {
+    const { email, password } = request.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) 
+     return response.status(400).send({ error: "User not found." });
+    
+    if (!await bcrypt.compare(password, user.password))
+     return response.status(400).send({ error: "Invalid password" });
+
+    // TO NOT RETURN THE PASSWORD
+    user.password = undefined;
+
+    response.send({ user });
   }
 
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,5 +5,6 @@ const authController = require('./controllers/authController');
 const routes = express.Router();
 
 routes.post('/auth/register', authController.register);
+routes.post('/auth/authenticate', authController.authenticate);
 
 module.exports = routes;
