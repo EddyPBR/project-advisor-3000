7c85a25d079b63fb7dea75f2dfef4a1ffd25778b
EddyPBR <edvaldojunior1310@gmail.com>
2020-04-20 11:34:12 -0300

    adding styles to card

--- frontend/src/pages/Projects/index.js
+++ frontend/src/pages/Projects/index.js
@@ -4,7 +4,8 @@ import './styles.scss';
 export default class home extends Component {
   render() {
     return (
-      <header class="project-header">
+      <>
+        <header class="project-header">
         <div className="header-row">
           <div class="f-col">
             <div class="row">
@@ -24,6 +25,32 @@ export default class home extends Component {
           </div>
         </div>
       </header>
+        <div class="cards-container">
+          <div className="cards-flex-grid">
+
+            <div className="card">
+              <div className="card-header">
+                <b class="title">Lorem ipsum dolor</b>
+                <div class="links">
+                  <a class="erase" href="#">ok</a>
+                  <a class="edit" href="#">ok</a>
+                </div>
+              </div>
+
+              <div class="card-content">
+                <div className="card-body">
+                  <p>Ultrices erat sit amet neque commodo vehicula. Etiam libero ante, finibus.</p>
+                </div>
+
+                <div className="card-footer">
+                  <a href="#">See Project</a>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </>
     );
   }
 }
