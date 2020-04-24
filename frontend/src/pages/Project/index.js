a4a690cffca45ed70198afaad49383a41ab32779
EddyPBR <edvaldojunior1310@gmail.com>
2020-04-24 20:24:53 -0300

    adding styles into page project

--- frontend/src/pages/Project/index.js
+++ frontend/src/pages/Project/index.js
@@ -48,60 +48,60 @@ export default class Project extends Component {
         </header>
 
         <div className="project-content">
-          <div className="container">
-            <div className="form-project">
+          <form className="form-project">
 
-              <form>
-                <div class="form-content">
-                  <div class="row">
-                    <label>Title</label>
-                    <input type="text" placeholder="Title of the project"></input>
-                  </div>
+            <div class="form-content">
+              <div class="row">
+                <label>Title</label>
+                <input type="text" placeholder="Title of the project"></input>
+              </div>
 
-                  <div class="row">
-                    <label>Description</label>
-                    <input type="text" placeholder="Description of your project"></input>
-                  </div>
+              <div class="row">
+                <label>Description</label>
+                <input
+                  type="text"
+                  placeholder="Description of your project"
+                ></input>
+              </div>
 
-                  <div class="row">
-                    <label>Tasks</label>                    
-                    <div class="row-add-task">
-                      <input type="text" placeholder="new Task" />
-                      <Link to="#" class="add-task">
-                        <FiPlus size={22} color="#F72222" />
-                        <span>Add</span>
-                      </Link>
-                    </div>
-                  </div>
-                  
+              <div class="row">
+                <label>Tasks</label>
+                <div class="row-add-task">
+                  <input type="text" placeholder="new Task" />
+                  <Link to="#">
+                    <FiPlus size={22} color="#7AF49C" />
+                    <span>Add</span>
+                  </Link>
                 </div>
-
-                <div className="form-tasks">
-                  <b>Tasks List</b>
-                  <div className="tasks-list">
-
-                    <div className="task-row">
-                      <input type="checkbox" id="Task" name="Task" />
-                      <label for="Task">Task</label>
-                    </div>
-
-                    <div className="task-row">
-                      <input type="checkbox" id="Task" name="new Task" />
-                      <label for="new Task">new Task</label>
-                      <FiEdit size={18} color="#F72222" />
-                      <FiTrash2 size={18} color="#F72222" />
-                    </div>
-
-                  </div>
-                </div>
-
-              </form>
-
+              </div>
             </div>
-          </div>
-        </div>
 
-      </>        
+            <div className="form-tasks">
+              <b>Tasks List</b>
+              <div className="tasks-list">
+                <div className="task-row">
+                  <label>
+                    <input type="checkbox" id="Task" name="new Task" />
+                    <span>Task</span>
+                  </label>
+                  <FiEdit size={18} color="#F4AF25" />
+                  <FiTrash2 size={18} color="#F72222" />
+                </div>
+
+                <div className="task-row">
+                  <label>
+                    <input type="checkbox" id="Task" name="new Task" />
+                    <span>New Task</span>
+                  </label>
+                  <FiEdit size={18} color="#F4AF25" />
+                  <FiTrash2 size={18} color="#F72222" />
+                </div>
+
+              </div>
+            </div>
+          </form>
+        </div>
+      </>
     );
   }
 }
